e0089b87135c6909e5934290f353bb8cd8bb65e9
aparkening <aaron.parkening@gmail.com>
2020-02-23 15:01:26 -0500

    Add find_or_create_by_omniauth and commented validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,30 @@ class User < ApplicationRecord
   has_many :user_favorites, dependent: :destroy
   has_many :events
   has_many :parks, through: :events
+
+  # Validations
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true, on: :create
+  # validates :password, length: { minimum: 6 }, on: :create
+
+  # Find or create user by Omniauth
+  def self.find_or_create_by_omniauth(auth)
+
+    # Create new user only if it doesn't exist
+    where(uid: auth.uid).first_or_initialize do |user|
+      user.uid ||= auth.uid
+      user.email = auth.info.email
+      # user.name = auth.info.name
+      # user.photo = auth.info.photo
+
+      if !user.password_digest
+        pass = SecureRandom.hex(30)
+        user.password = pass
+        # user.password_confirmation = pass
+      end
+
+      user.save
+    end
+  end
+
 end

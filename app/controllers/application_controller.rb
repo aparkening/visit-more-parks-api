da36cfdb580a1b681fc1188a77b6ddbfc2289220
aparkening <aaron.parkening@gmail.com>
2020-02-23 17:06:20 -0500

    Remove extraneous protect_from_forgery.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :resource_error

0e541b1ccf8d299ba265a27a7800ef1f1702a5a0
aparkening <aaron.parkening@gmail.com>
2020-02-23 15:20:03 -0500

    Add Park fetch and create.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'httparty'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Get parks from National Park Service api
+response = HTTParty.get("https://developer.nps.gov/api/v1/parks?limit=1000&api_key=#{ENV['NPS_API_KEY']}", format: :plain)
+parsed = JSON.parse response, symbolize_names: true
+
+# binding.pry
+
+# Create park objects from api response
+parsed[:data].each do |park| 
+  Park.create(
+    npsId: park[:id],
+    latLong: park[:latLong],
+    description: park[:description],
+    designation: park[:designation],
+    parkCode: park[:parkCode],
+    fullName: park[:fullName],
+    url: park[:url]
+  )
+end

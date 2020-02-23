7cefbf00323bab27deeb764e2f18fae627cfd83e
aparkening <aaron.parkening@gmail.com>
2020-02-23 17:01:21 -0500

    Add protect_from_forgery.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   rescue_from ActiveRecord::RecordInvalid, with: :resource_error

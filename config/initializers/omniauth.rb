d06e543a059380db47e4f09cf4f52977357750a1
aparkening <aaron.parkening@gmail.com>
2020-02-24 11:49:33 -0500

    Add new scope, access_type, prompt, and select_account.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
     {
-      scope: 'userinfo.email, userinfo.profile',
-      prompt: 'select_account',
+      scope: 'userinfo.email, userinfo.profile, calendar.events',
+      access_type: 'offline',
+      prompt: 'consent',
       image_aspect_ratio: 'square',
-      image_size: 50
+      image_size: 50,
+      select_account: true
     }
   end

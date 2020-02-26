ef06736ec19e11d730331827f25ed6f2d512428e
aparkening <aaron.parkening@gmail.com>
2020-02-25 19:00:42 -0500

    Add address, latitude, and longitude to main migration.

--- db/migrate/20200223195616_create_parks.rb
+++ db/migrate/20200223195616_create_parks.rb
@@ -3,11 +3,14 @@ class CreateParks < ActiveRecord::Migration[6.0]
     create_table :parks do |t|
       t.string :npsId
       t.string :latLong
+      t.float :latitude
+      t.float :longitude
       t.string :description
       t.string :designation
       t.string :parkCode
       t.string :fullName
       t.string :url
+      t.string :address
 
       t.timestamps
     end

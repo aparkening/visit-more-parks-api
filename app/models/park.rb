99fd1cedb609efc1dd3f65145afefb91dbac3333
aparkening <aaron.parkening@gmail.com>
2020-02-25 19:02:16 -0500

    Add geocoded_by :address to fix geocode errors.

--- app/models/park.rb
+++ app/models/park.rb
@@ -4,6 +4,7 @@ class Park < ApplicationRecord
   has_many :events
   has_many :users, through: :events
 
+  geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
 

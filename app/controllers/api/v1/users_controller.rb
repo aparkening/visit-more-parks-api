580ad5f43002b8a645e362eb1448363a49ee9100
aparkening <aaron.parkening@gmail.com>
2020-02-26 15:38:36 -0500

    Remove create and user_params methods.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-  # Create user
-  def create
-    # binding.pry
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      cookies["logged_in"] = true
-      render json: user, except: [:password_digest]
-    else
-      render json: { errors: user.errors.full_messages }
-    end
-  end
-
-  # Delete user
+  # Delete record
   def destroy
     user = User.find(params[:id])
 
@@ -27,11 +14,4 @@ class Api::V1::UsersController < ApplicationController
     end
   end
   
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
-
 end

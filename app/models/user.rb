93950c288883b7936fdf3762e00e4724a9015e27
aparkening <aaron.parkening@gmail.com>
2020-02-26 11:10:13 -0500

    Move google_token and google_refresh_token outside of first_or_initialize method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,23 +20,22 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.name = auth.info.name
       user.photo = auth.info.image
-
-      # Access_token is used to authenticate request made from this app to the google server
-      user.google_token = auth.credentials.token
       
-      # Refresh_token to request new access_token.
-      # Note: Refresh_token is only sent once during the first request
-      refresh_token = auth.credentials.refresh_token
-      user.google_refresh_token = refresh_token if refresh_token.present?
-
       if !user.password_digest
         pass = SecureRandom.hex(30)
         user.password = pass
         # user.password_confirmation = pass
       end
-
-      user.save
     end
+
+    # Access_token is used to authenticate request made from this app to the google server
+    user.google_token = auth.credentials.token
+    
+    # Refresh_token to request new access_token.
+    refresh_token = auth.credentials.refresh_token
+    user.google_refresh_token = refresh_token if refresh_token.present?
+
+    user.save
   end
 
 end

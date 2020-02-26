d0da7b325a52e71d35f34df0c21b8c00151c36d7
aparkening <aaron.parkening@gmail.com>
2020-02-26 15:22:14 -0500

    Add favorite and unfavorite methods.

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,11 +1,38 @@
 class Api::V1::ParksController < ApplicationController
+  before_action :authenticate
 
-  # All Parks
+  # All parks
   def index
     # Order parks by lattitude 
     parks = Park.all.order(latLong: :asc)
     # Return parks as json
     render json: { parks: parks }
+  end
+
+  # Favorite park
+  def favorite
+
+binding.pry
+
+    fav = current_user.user_favorites.create(params[:park_id])
+    render json: { favorite: fav.id}
+  end
+
+  # Delete record
+  def unfavorite
+
+binding.pry
+
+    fav = UserFavorite.find(param[:id])
+
+    # Only owner can delete
+    authorize_resource(fav)
+
+    # Delete database event
+    fav.destroy
+
+    # Render json
+    render json: { favorite: fav.id }  
   end
 
 end

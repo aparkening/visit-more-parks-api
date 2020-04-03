00c64b4b63e682ffd80ea3e40b63f33d5e900b15
aparkening <aaron.parkening@gmail.com>
2020-04-03 16:10:20 -0400

    Order parks by fullName. Remove pry from favorite and unfavorite methods. Comment more code.

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -3,26 +3,22 @@ class Api::V1::ParksController < ApplicationController
 
   # All parks
   def index
-    # Order parks by lattitude 
-    parks = Park.all.order(latLong: :asc)
+    # Order parks by name 
+    parks = Park.all.order(fullName: :asc)
     # Return parks as json
     render json: { parks: parks }
   end
 
   # Favorite park
   def favorite
-
-binding.pry
-
+    # Create favorite based on park id
     fav = current_user.user_favorites.create(params[:park_id])
     render json: { favorite: fav.id}
   end
 
   # Delete record
   def unfavorite
-
-binding.pry
-
+    # Find favorite
     fav = UserFavorite.find(param[:id])
 
     # Only owner can delete
